23a30ed71a0e97cb7743ca788b5cf50795f98005
Yuki Nakayama <yuki.nkym.me@gmail.com>
2023-09-07 14:44:33 +0900

    declaring dataTypes

---
+++ 1-3.java
@@ -0,0 +1,14 @@
+/*データ型について */
+
+class DataType{
+    public static void main(String[] args){
+        int age = 20;
+        float weight = 63.0f;
+        double height = 168.5;
+        char bloodType = 'A';
+        String name = "Marry";
+        System.out.println("Hello, I am " + name);
+        System.out.println("I am " + age + " years old");
+        System.out.println("I am " + height + "cm tall, and weight of " + weight + "kg. My bloodtype is " + bloodType);
+    }
+}
