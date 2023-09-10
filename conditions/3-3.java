0a3f7b7819ee4975f55b4959c7434ec805632ac6
Yuki Nakayama <yuki.nkym.me@gmail.com>
2023-09-10 10:31:20 +0900

    while loop

---
+++ conditions/3-3.java
@@ -0,0 +1,11 @@
+/* while文 */
+
+class While{
+    public static void main(String[] args){
+        int i = 0;
+        while(i < 3){
+            System.out.println("i = " + i);
+            i ++;
+        }
+    }
+}
