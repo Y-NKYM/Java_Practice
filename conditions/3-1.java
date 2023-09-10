edc5cb3c5ce38836976a928536e90e2854e766b6
Yuki Nakayama <yuki.nkym.me@gmail.com>
2023-09-10 09:58:01 +0900

    if-else condition

---
+++ conditions/3-1.java
@@ -0,0 +1,19 @@
+/* if文 */
+
+class Branch{
+    public static void main(String[] args){
+        int price = Integer.parseInt(args[0]);
+        double rate = 0.10;
+        int discount, amount;
+        
+        if (price >= 3000){  //3000以上の場合
+            discount = 300;
+        } else {
+            discount = 0;
+        }
+
+        amount = (int)((price-discount) * (1 + rate));
+        System.out.println("値引価格：" + discount + "円");
+        System.out.println("税込価格：" + amount + "円");
+    }
+}
