c5b69c291c59791653d61ad61b9c32946763d538
Yuki Nakayama <yuki.nkym.me@gmail.com>
2023-09-11 10:43:26 +0900

    defining object oriented propoerty/method

---
+++ object-oriented/4-1.java
@@ -0,0 +1,25 @@
+/* オブジェクト指向について */
+
+class Student{
+    //変数（属性）の定義
+    String name;
+    int engScore;
+    int mathScore;
+
+    //
+    void display(){
+        System.out.println(name + "さん");
+        System.out.println("英語：" + engScore + "点");
+        System.out.println("数学：" + mathScore + "点");
+    }
+
+    void setScore(int eng, int math){
+        engScore = eng;
+        mathScore = math;
+    }
+
+    double getAvg(){
+        double avg = (engScore + mathScore) / 2.0;
+        return avg;
+    }
+}
