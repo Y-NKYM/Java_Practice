a324046f143ad75dd6a836851edbc16be90c2042
Yuki Nakayama <yuki.nkym.me@gmail.com>
2023-09-13 21:52:44 +0900

    practice problem 2

---
+++ object-oriented/4-practice3.java
@@ -0,0 +1,39 @@
+class Car2{
+    int no;
+    int speed;
+
+    Car2(){
+        no = 0;
+    }
+
+    Car2(int n){
+        no = n;
+    }
+
+    void run(int s){
+        speed = s;
+    }
+
+    void brake(){
+        speed = 0;
+    }
+
+    void brake(int s){
+        speed -= s;
+    }
+
+    void display(){
+        System.out.println("ナンバー" + no + "の速度は" + speed + "です");
+    }
+
+    public static void main(String[] args){
+        Car2 c1 = new Car2();
+        c1.run(30);
+        c1.display();
+
+        Car2 c2 = new Car2(2525);
+        c2.run(30);
+        c2.brake(10);
+        c2.display();
+    }
+}
