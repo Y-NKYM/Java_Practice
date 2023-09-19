37d37847000d5f637d8490aed32cb8ee35e04a42
Yuki Nakayama <yuki.nkym.me@gmail.com>
2023-09-19 14:29:03 +0900

    defining "package"

---
+++ package/StuSample.java
@@ -0,0 +1,10 @@
+/* インポート */
+
+import pack.Student;
+public class StuSample{
+    public static void main(String[] args){
+        Student student = new Student("山田");
+        student.display();
+    }
+}
+
