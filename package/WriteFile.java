2ad1156898472d7f9561a634dddcb39e8987eba3
Yuki Nakayama <yuki.nkym.me@gmail.com>
2023-09-19 15:20:24 +0900

    java.io practice code

---
+++ package/WriteFile.java
@@ -0,0 +1,20 @@
+/* output.txtに記述した文字をprintするコード */
+
+import java.io.*;
+public class WriteFile{
+    public static void main(String[] args){
+        try {
+            BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+            PrintWriter w = new PrintWriter(new BufferedWriter(new FileWriter("output.txt")));
+
+            String str;
+            while((str = r.readLine()) != null){
+                w.println(str);
+            }
+            r.close();
+            w.close();
+        } catch (IOException e){
+            System.out.println(e);
+        }
+    }
+}
