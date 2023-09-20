33c33f3fb8b10a5b047eaa435cac5e26cc0b2352
Yuki Nakayama <yuki.nkym.me@gmail.com>
2023-09-20 21:00:35 +0900

    practice chapter 5-2: interface

--- package/5-practice2.java
+++ package/5-practice2.java
@@ -3,13 +3,20 @@ abstract class Vehicle{
     public abstract void showCrewNum();
 }
 
-class Taxi extends Vehicle{
+interface Stopable{
+    public abstract void stop();
+}
+
+class Taxi extends Vehicle implements Stopable{
     // private int crewNum;
     public Taxi(int crewNum){
         this.crewNum = crewNum;
     }
     public void showCrewNum(){
         System.out.println("タクシー乗車人数：" + crewNum + "名");
+    }
+    public void stop(){
+        System.out.println("ここで降ります");
     }
 }
 
@@ -23,11 +30,15 @@ class Train extends Vehicle{
     }
 }
 
-class Bus extends Vehicle{
+class Bus extends Vehicle implements Stopable{
     public Bus(int crewNum){
         this.crewNum = crewNum;
     }
     public void showCrewNum(){
         System.out.println("バス乗車人数：" + crewNum + "名");
     }
+    public void stop(){
+        System.out.println("ここで降ります");
+    }
 }
+
