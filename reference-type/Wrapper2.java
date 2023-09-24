1984702924394ea6bcfe807f90ca6b8b70d6d2fc
Yuki Nakayama <yuki.nkym.me@gmail.com>
2023-09-24 20:59:27 +0900

    defining Integer.MAX_VALUE MIN_VALUE

---
+++ reference-type/Wrapper2.java
@@ -0,0 +1,14 @@
+public class Wrapper2{
+    public static void main(String[] args){
+        try{
+            int i = Integer.parseInt(args[0]);
+            String s = Integer.toBinaryString(i);
+            System.out.println("二進数に変換：" + s);
+        } catch(NumberFormatException e){
+            System.out.print(Integer.MIN_VALUE);
+            System.out.print("から");
+            System.out.print(Integer.MAX_VALUE);
+            System.out.print("までの数値を入力してください");
+        }
+    }
+}
