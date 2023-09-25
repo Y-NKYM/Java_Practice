da371ba034c1880804baf6598b73e7540ad1a09e
Yuki Nakayama <yuki.nkym.me@gmail.com>
2023-09-25 21:05:32 +0900

    practice chapter reference-type_1 改良

--- reference-type/practice/NameArray.java
+++ reference-type/practice/NameArray.java
@@ -2,14 +2,15 @@ public class NameArray{
     public static void main(String[] args){
         String[] names = {"桜井","田原","中川","鈴木"};
         String search = args[0];
-        boolean result = false;
-
-        for(int i = 0; i < names.length; i++){
+        int i;
+        for(i = 0; i < names.length; i++){
             if(names[i].equals(search)){
-                result = true;
+                break;
             }
         }
-        if(result){
+        /* まだiがfor loop範囲内の場合breakが実行されたという
+           意味になるため、含まれているということがわかる。*/
+        if(i < names.length){  
             System.out.println(search + "さんは含まれています");
         } else {
             System.out.println(search + "さんは含まれていません");
